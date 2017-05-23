7d45fcf1afc43de0390e6daafecf37ee4ec47c6a
JYChoi <neocjy@naver.com>
2017-05-23 23:07:37 +0900

    fdsfsf

--- js/testServer.js
+++ js/testServer.js
@@ -1,25 +1,13 @@
-var express = require("express"),
-    users = require("./routes/users");
-
-var serverPort = 3000;
 var socketPort = 999;
 
 var io = require("socket.io").listen(socketPort);
-var app = express();
-
-app.get('/users', users.findAll);
-app.post('/users',users.addUser);
-
-
-//web 서버 port설정필요
-app.listen(serverPort);
-
 console.log('webserver running on port' + serverPort);
 
 io.sockets.on("connection", function (socket) {
     console.log("a user connected");
     socket.emit('message', 'web server connected');
-    socket.on('open', function (data) {
-        io.sockets.emit('rMsg', data);
+
+    socket.on("Save", function (data) {
+        console.log(data);
     })
 });
